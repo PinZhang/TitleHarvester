9091f95e48cc7adbdd76d3bbaafbe962fdbd055d
Pin Zhang <pzhang@mozilla.com>
2014-01-07 12:56:06 +0800

    Fix output dir

--- scripts/weka/generate_model.sh
+++ scripts/weka/generate_model.sh
@@ -17,7 +17,7 @@ echo $COMMAND
 $COMMAND
 
 echo "Step 3: generate json from nbays result"
-COMMAND="./utils/prob2json.py $DATA_DIR/$DATA_DIR.nbayes.results.w$NUM_WORDS.txt testoutput/model.$NUM_WORDS.js"
+COMMAND="./utils/prob2json.py $DATA_DIR/$DATA_DIR.nbayes.results.w$NUM_WORDS.txt $DATA_DIR/model.$NUM_WORDS.js"
 echo $COMMAND
 $COMMAND
 
